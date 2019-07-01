5dccc6b642e1850601eae520dc4399a87b0048a3
Charles Kettell <charleskettell@hotmail.co.uk>
2019-07-01 17:07:37 +0100

    minimum added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,13 +3,19 @@ class Oystercard
   attr_reader :balance
 
   LIMIT = 90
+  MINIMUM = 1
 
   def limit
     LIMIT
   end
 
+  def minimum
+    MINIMUM
+  end
+
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(load)
@@ -21,9 +27,26 @@ class Oystercard
     @balance -= amount
   end
 
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    fail "Your Oystercard has less than £#{MINIMUM}" if limit_reached?
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
   private
 
   def limit_exceeded?(load)
     load > LIMIT - @balance
+  end
+
+  def limit_reached?
+    @balance < MINIMUM
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Oystercard do
     it 'Sets balance to zero' do
       expect(subject.balance).to eq(0)
     end
+    it 'it initialises in_journey to false' do
+      expect(subject).not_to be_in_journey
+    end
   end
   describe '#top_up' do
     it "adds money to your card" do
@@ -21,6 +24,22 @@ RSpec.describe Oystercard do
     it "deduces money from your card" do
       subject.top_up(10)
       expect(subject.deduce(10)).to eq 0
+    end
+  end
+
+  describe '#touch_in' do
+    it 'Sets in_journey to true' do
+      subject.top_up(subject.minimum)
+      expect(subject.touch_in).to eq true
+    end
+    it "raises error if balance is less than £1" do
+      expect { subject.touch_in }.to raise_error "Your Oystercard has less than £#{subject.minimum}"
+    end
+  end
+
+  describe '#touch_out' do
+    it 'Sets in_journey to false' do
+      expect(subject.touch_out).to eq false
     end
   end
 end
