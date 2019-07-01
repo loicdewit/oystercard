4c1127046e7669c4fac107baf7e17542a2088865
Loïc de Wit <dew.loic@gmail.com>
2019-07-01 16:16:50 +0100

    steps 4 through 7

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,29 @@
 class Oystercard
 
-  attr_reader :balance 
+  attr_reader :balance
+
+  LIMIT = 90
+
+  def limit
+    LIMIT
+  end
 
   def initialize
     @balance = 0
   end
 
+  def top_up(load)
+    fail "Card limit is #{LIMIT}, your balance is #{@balance}" if limit_exceeded?(load)
+    @balance += load
+  end
 
+  def deduce(amount)
+    @balance -= amount
+  end
+
+  private
+
+  def limit_exceeded?(load)
+    load > LIMIT - @balance
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,10 +1,26 @@
 require 'oystercard'
 
-
 RSpec.describe Oystercard do
   describe '#initialize' do
     it 'Sets balance to zero' do
-      expect( subject.balance).to eq(0)
+      expect(subject.balance).to eq(0)
+    end
+  end
+  describe '#top_up' do
+    it "adds money to your card" do
+      subject.top_up(5)
+      expect(subject.balance).to eq(5)
+    end
+    it "prevents card from exceeding maximum amount of pounds" do
+      allowed = (subject.limit - subject.balance)
+      test_amount =  allowed + 1
+      expect { subject.top_up(test_amount) }.to raise_error "Card limit is #{subject.limit}, your balance is #{subject.balance}"
+    end
+  end
+  describe '#deduct' do
+    it "deduces money from your card" do
+      subject.top_up(10)
+      expect(subject.deduce(10)).to eq 0
     end
   end
 end
