1f1dce3ddb21f9ee0671c67bacb18aa49f085b17
Charles Kettell <charleskettell@hotmail.co.uk>
2019-07-01 15:20:44 +0100

    balance set to zero

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
-RSpec.describe Oystercard do
+require 'oystercard'
 
+
+RSpec.describe Oystercard do
+  describe '#initialize' do
+    it 'Sets balance to zero' do
+      expect( subject.balance).to eq(0)
+    end
+  end
 end
